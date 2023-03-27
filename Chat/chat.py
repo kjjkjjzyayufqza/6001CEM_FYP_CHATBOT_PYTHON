e4690b9f29b84a0af316bbccd7c554b03cc0020a
kenkwanatgai <kjjkjjzyayufqza@yahoo.com>
2023-03-27 19:46:25 +0800

    fix bug

---
+++ Chat/chat.py
@@ -0,0 +1,89 @@
+import random
+import json
+import torch
+import os
+
+import torch.nn as nn
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    
+    #set bag to all 0
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+            
+    return bag
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size) 
+        self.l2 = nn.Linear(hidden_size, hidden_size) 
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        out = self.relu(out)
+        out = self.l3(out)
+        # no activation and no softmax at the end
+        return out
+
+
+def ChatBot(Message):
+    current_path = os.path.dirname(__file__) + '/'
+    with open(current_path + 'intents.json', 'r') as f:
+        intents = json.load(f)
+
+    FILE = "data.pth"
+    data = torch.load(current_path + FILE)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
+    sentence = Message
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+    
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.98:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                return (f"{random.choice(intent['responses'])}")
+    else:
+        return (f"I do not understand...")
+
+if __name__ == '__main__':
+    ChatBot()
--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from typing import Union
 from fastapi.responses import RedirectResponse
 from fastapi import FastAPI
-
 from pydantic import BaseModel
-
+import sys
+sys.path.append(r'.\Chat')
+import Chat.chat
+import CPUinfo.CPUinfo
 
 app = FastAPI()
+
+def FirstStartFunc():
+    CPUinfo.CPUinfo.GetCPUinfo()
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+    
+class MessageBody(BaseModel):
+    MessageStr: str
 
 @app.get("/")
 async def docs_redirect():
     return RedirectResponse(url='/docs')
 
 @app.post("/Chat")
-def read_item(Message: str):
-    return "hi"
+def read_item(Message: MessageBody):
+    return Chat.chat.ChatBot(Message.MessageStr)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -27,3 +35,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+
+
+
+
+FirstStartFunc()
